ef10bc80ba14e79e8f3aac46897d35661c97a67b
David Lawes <david.lawes@guardian.co.uk>
2021-10-01 16:16:03 +0100

    add redux and createStore

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
-import React, { useState } from "react";
+import React from "react";
+import { createStore } from "redux";
 import HelloWorld from "./HelloWorld";
+import reducer from './reducers'
+
+// { dispatch, subscribe, getState }
+// reducer takes 2 args: state, action
+
+const initialState = { tech: 'react' };
+const store = createStore(reducer, initialState);
+
+console.log(store.getState())
+
+// store.dispatch(action)
+
+// store.getState = { new state }
 
 const App = () => {
-  const [tech] = useState("React");
-  return <HelloWorld tech={tech} />;
+
+  // return <HelloWorld tech={'redux'} />;
+  return <HelloWorld tech={store.getState().tech} />;
 };
 
 export default App;
---
+++ src/reducers/index.js
@@ -0,0 +1,4 @@
+export default (state, action) => {
+    // redux action is a plain object
+    return state;
+}
